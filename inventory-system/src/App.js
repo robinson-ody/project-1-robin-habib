691b4ad4028401968a23307a08e7835007033ce0
Robin <robin071196@gmail.com>
2018-09-24 01:25:17 +0700

    AJAX Based
    - Creating login page with AJAX
    - Added icon too
    - Changed the title from JS Script

---
+++ inventory-system/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    document.title = "Login Page | Blibli Inventory System"
+    
+    
+    return (
+      <div className="App">
+
+        <div id="container">
+          <div id="container-left">
+          </div>
+
+          <div id="container-right">
+            <p>Please enter your username and password to login</p>
+
+            <form action="" method="post">
+              <input type="text" name="username" placeholder="Username..." />
+              <input type="password" name="password" placeholder="Password..." />
+              <button type="submit" className="btn-login">Log In</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
