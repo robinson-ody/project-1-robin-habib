ecddc0d41fd7dcbdc4e9b1d65d95920c98efc5ea
Robin <robin071196@gmail.com>
2018-12-10 00:53:18 +0700

    Restrukturisasi File dan Delete Function

--- Front-End/src/views/Employee/Employee.js
+++ Front-End/src/views/Employee/Employee.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import Header from "../../components/Header/Header";
-import TableSingle from '../../components/TableSingle/TableSingle';
 import axios from 'axios';
+import addNewBtn from "../../elements/add-new-btn.png";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import trashIcon from "../../elements/trash-copy-8.png";
+import './Employee.css';
 
 export default class Employee extends React.Component{
     constructor(props) {
+        super(props);
+        this.state = {
+            data : []
+        }
+    }
+
+    componentDidMount(){
+      this.getData();
+    };
+
+    getData(){
         axios.get('http://localhost:8080/api/employee')
             .then(res => {
                 this.setState({data : res.data})
@@ -13,22 +27,72 @@ export default class Employee extends React.Component{
             .catch(function(error) {
                 console.log(error);
             });
+    }
 
-        super(props);
-        this.state = {
-            data : [],
-            dummy : [
-                {employeeId : '20181208', name: 'Muhammad Habib', birthday: '7 November 1996', gender: 'Male', division: 'Technology', superior: 'Anthonius'}
-            ]
-        }
+    // changeFormat(e){
+    //     let hasil = '';
+    //   for(let i = 0 ; i <= e.length ; i++){
+    //       if (e.substr(i, 1) === '-') {
+    //           hasil += '/'
+    //       } else {
+    //           hasil += e.substr(i, 1)
+    //       }
+    //   }
+    //   return hasil
+    // };
+
+    deleteHandler(id){
+        axios.delete('http://localhost:8080/api/employee/' + id)
+            .then(()=> this.getData())
     }
 
     render(){
         return(
             <div>
                 <Header pageName="Employee" />
-                <TableSingle title='Employee' version='employee' placeholder='Search Employees...'
-                             data={this.state.dummy}/>
+
+                <div className='tableSingle'>
+                    <div className='tableHeader'>
+                        <div className='tableTitle'>Employee List</div>
+
+                        <img src={addNewBtn} alt='Add New Button' className='addNewBtn' title='Add New Product' />
+
+                        <SearchBar placeholder='Search Employee...' />
+                    </div>
+
+                    <div className='tableBody'>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th className='chkbox'><input type='checkbox' /></th>
+                                    <th className='inventory'>Employee ID</th>
+                                    <th className='name'>Name</th>
+                                    <th className='birthday'>Birthday</th>
+                                    <th className='gender'>Gender</th>
+                                    <th className='division'>Division</th>
+                                    <th className='superior'>Superior</th>
+                                    <th className='deleteIcon'/>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {this.state.data.map((item, index)=>(
+                                    <tr key={index}>
+                                        <td className='chkbox'><input type='checkbox' /></td>
+                                        <td className='inventory'>{item.id}</td>
+                                        <td className='name'>{item.name}</td>
+                                        <td className='birthday'>{item.birthday}</td>
+                                        <td className='gender'>{item.gender}</td>
+                                        <td className='division'>{item.division}</td>
+                                        <td className='superior'>{item.superior}</td>
+                                        <td className='deleteIcon' onClick={()=> this.deleteHandler(item.id)}><img src={trashIcon} width='15px' alt='Trash icon' /></td>
+                                    </tr>
+                                ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         )
     }
--- Front-End/src/views/Inventory/Inventory.js
+++ Front-End/src/views/Inventory/Inventory.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Header from '../../components/Header/Header'
-import TableSingle from "../../components/TableSingle/TableSingle";
+import './Inventory.css';
 import axios from 'axios';
+import image from "../../elements/black.png";
+import trashIcon from "../../elements/trash-copy-8.png";
+import addNewBtn from "../../elements/add-new-btn.png";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import PrintIcon from "../../elements/print-icon.png";
 
 export default class Inventory extends React.Component{
 	constructor(props){
+        super(props);
+		this.state = {
+			data : []
+		}
+	}
+
+	componentDidMount() {
+	  this.getData()
+    };
+
+	getData(){
         axios.get('http://localhost:8080/api/inventory')
             .then(res => {
                 this.setState({data : res.data})
@@ -13,19 +29,70 @@ export default class Inventory extends React.Component{
             .catch(function(error) {
                 console.log(error);
             });
+    }
 
-        super(props);
-		this.state = {
-			data : []
-		}
-	}
+    deleteHandler(id){
+        axios.delete('http://localhost:8080/api/inventory/' + id)
+            .then(()=> this.getData())
+    }
 
-	render(){
+    render(){
 		return(
 			<div>
 				<Header pageName="Inventory" />
-                <TableSingle title='Inventory' version='inventory' placeholder='Search Items...'
-							 data={this.state.data}/>
+
+                <div className='tableSingle'>
+                    <div className='tableHeader'>
+                        <div className='tableTitle'>Inventory List</div>
+
+                        <img src={addNewBtn} alt='Add New Button' className='addNewBtn' title='Add New Product' />
+
+                        <SearchBar placeholder='Seach Item...' />
+
+                        <div id='assignInventory' className='assignInventory'>
+                        <span className='tulisanAssignInventory'>
+                            Request Item
+                        </span>
+                        </div>
+
+                        <img src={PrintIcon} id='printIcon' className='printIcon' alt='Print Icon' />
+                    </div>
+
+                    <div className='tableBody'>
+                        <table>
+                            <thead>
+                            <tr>
+                                <th className='chkbox'><input type='checkbox' /></th>
+                                <th className='inventory'>Inventory ID</th>
+                                <th className='detail'>Detail</th>
+                                <th className='stock'>Stock</th>
+                                <th className='available'>Available</th>
+                                <th className='price'>Price</th>
+                                <th className='productImage'>Image</th>
+                                <th className='seeUsers'/>
+                                <th className='deleteIcon'/>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            {this.state.data.map((item, index)=>(
+                                <tr key={index}>
+                                    <td className='chkbox'><input type='checkbox' /></td>
+                                    <td className='inventory'>{item.inventoryId}</td>
+                                    <td className='detail'>{item.detail}</td>
+                                    <td className='stock'>{item.stock}</td>
+                                    <td className='available'>{item.available}</td>
+                                    <td className='price'>Rp <span className='moneyValue'>{item.price.toLocaleString()}</span></td>
+                                    <td className='productImage'><img src={image} width='20px' alt='Product'/></td>
+                                    <td className='seeUsers'>See Users</td>
+                                    <td className='deleteIcon' onClick={()=> this.deleteHandler(item.id)}><img src={trashIcon} width='15px' alt='Trash icon' /></td>
+                                </tr>
+                            ))
+                            }
+                            </tbody>
+						</table>
+                    </div>
+                </div>
 			</div>
 		)
 	}
