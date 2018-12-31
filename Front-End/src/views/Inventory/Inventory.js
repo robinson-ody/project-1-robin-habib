7c06676f75021455b0765a823f69764834f8c213
Robin <robin071196@gmail.com>
2018-12-31 17:51:45 +0700

    Pull

--- Front-End/src/views/Inventory/Inventory.js
+++ Front-End/src/views/Inventory/Inventory.js
@@ -64,12 +64,10 @@ export default class Inventory extends React.Component{
 
     addRequest(id){
 	    let dataTemp = this.state.selected;
-        axios.get('http://localhost:8080/api/inventory/' + id)
-            .then(res => dataTemp.push(res.data));
 
-        this.setState({
-            selected: dataTemp
-        })
+	    axios.get('http://localhost:8080/api/inventory/' + id)
+            .then(res => {dataTemp.push(res.data)})
+            .then(()=> {this.setState({selected: dataTemp})})
     }
 
     addWindowOn(){
@@ -97,7 +95,7 @@ export default class Inventory extends React.Component{
             .catch(error => {console.log(error)})
     };
 
-    thousandSeparator(e){
+    static thousandSeparator(e){
         let result = '';
         let counter = 0;
         for (let i = e.toString().length ; i >= 0 ; i--){
@@ -107,122 +105,136 @@ export default class Inventory extends React.Component{
                 result = e.toString().substr(i, 1) + result;
             }
             counter += 1;
-            console.log(result)
         }
         return result
     };
 
+    // Selected(){
+    // };
+
     render(){
         document.title = "Inventory | Blibli Inventory System";
 
-        return(
-			<div>
-                <div ref={this.blackBg} className='blackBg' />
+        return <div>
+            <div ref={this.blackBg} className='blackBg'/>
 
-                {/*WINDOW UNTUK REQUEST ITEM*/}
-                <div ref={this.tableRequest} className='tableRequest'>
-                    <div className='tableHeader'>
-                        <div className='tableTitle'>Requested Item</div>
-                        <img src={okBtn} alt='Ok Button' className='confirmationBtn' onClick={()=> this.submitRequest()} />
-                        <img src={cancelBtn} alt='Cancel Button' className='confirmationBtn' onClick={()=> this.requestWindowOff()} />
-                    </div>
+            {/*WINDOW UNTUK REQUEST ITEM*/}
+            <div ref={this.tableRequest} className='tableRequest'>
+                <div className='tableHeader'>
+                    <div className='tableTitle'>Requested Item</div>
+                    <img src={okBtn} alt='Ok Button' className='confirmationBtn' onClick={() => this.submitRequest()}/>
+                    <img src={cancelBtn} alt='Cancel Button' className='confirmationBtn'
+                         onClick={() => this.requestWindowOff()}/>
+                </div>
 
-                    <div className='tableBody'>
-                        <table>
-                            <thead>
-                            <tr>
-                                <th className='detail'>Detail</th>
-                                <th className='stock'>Stock</th>
-                            </tr>
-                            </thead>
+                <div className='tableBody'>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th className='detail'>Detail</th>
+                            <th className='stock'>Stock</th>
+                        </tr>
+                        </thead>
 
-                            <tbody>
-                            {this.state.selected.map((item, index)=>(
+                        <tbody>
+                            {this.state.selected.map((item, index) => (
                                 <tr key={index}>
                                     <td className='detail'>{item.detail}</td>
                                     <td className='stock'>{item.stock}</td>
                                 </tr>
-                            ))
-                            }
-                            </tbody>
-                        </table>
-                    </div>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
-                {/*END OF WINDOW UNTUK REQUEST ITEM*/}
+            </div>
+            {/*END OF WINDOW UNTUK REQUEST ITEM*/}
 
-                {/*WINDOW UNTUK ADD ITEM*/}
-                <div ref={this.tableAdd} className='tableAdd'>
-                    <div className='tableHeader'>
-                        <div className='tableTitle'>Add New Item</div>
-                        <img src={okBtn} alt='Ok Button' className='confirmationBtn' onClick={()=> this.addNewInventory()} />
-                        <img src={cancelBtn} alt='Cancel Button' className='confirmationBtn' onClick={()=> this.addWindowOff()} />
-                    </div>
-
-                    <div className='tableBody'>
-                        <img src={addNewPlaceholder} className="placeholder-image" alt="Add New"/>
-                        <input value={this.state.inventoryId} onChangeCapture={this.valueSetter.bind(this)} name='inventoryId' type='text' placeholder='Inventory ID'/>
-                        <input value={this.state.detail} onChangeCapture={this.valueSetter.bind(this)} name='detail' type='text' placeholder='Detail'/>
-                        <input value={this.state.price} onChangeCapture={this.valueSetter.bind(this)} name='price' type='text' placeholder='Price'/>
-                        <input value={this.state.stock} onChangeCapture={this.valueSetter.bind(this)} name='stock' type='text' placeholder='Total Items' style={{width:'80px'}}/> <span className='pcsWord'>Pcs</span>
-                    </div>
+            {/*WINDOW UNTUK ADD ITEM*/}
+            <div ref={this.tableAdd} className='tableAdd'>
+                <div className='tableHeader'>
+                    <div className='tableTitle'>Add New Item</div>
+                    <img src={okBtn} alt='Ok Button' className='confirmationBtn'
+                         onClick={() => this.addNewInventory()}/>
+                    <img src={cancelBtn} alt='Cancel Button' className='confirmationBtn'
+                         onClick={() => this.addWindowOff()}/>
                 </div>
-                {/*END OF WINDOW UNTUK ADD ITEM*/}
 
-                <Header pageName="Inventory" name={this.props.name}/>
+                <div className='tableBody'>
+                    <img src={addNewPlaceholder} className="placeholder-image" alt="Add New"/>
+                    <input value={this.state.inventoryId} onChangeCapture={this.valueSetter.bind(this)} name='inventoryId'
+                           type='text' placeholder='Inventory ID'/>
+                    <input value={this.state.detail} onChangeCapture={this.valueSetter.bind(this)} name='detail' type='text'
+                           placeholder='Detail'/>
+                    <input value={this.state.price} onChangeCapture={this.valueSetter.bind(this)} name='price' type='text'
+                           placeholder='Price'/>
+                    <input value={this.state.stock} onChangeCapture={this.valueSetter.bind(this)} name='stock' type='text'
+                           placeholder='Total Items' style={{width: '80px'}}/> <span className='pcsWord'>Pcs</span>
+                </div>
+            </div>
+            {/*END OF WINDOW UNTUK ADD ITEM*/}
 
-                <div className='tableSingle'>
-                    <div className='tableHeader'>
-                        <div className='tableTitle'>Inventory List</div>
+            <Header pageName="Inventory" name={this.props.name}/>
 
-                        <img src={addNewBtn} alt='Add New Button' className='addNewBtn' title='Add New Product' onClick={()=> this.addWindowOn()}/>
+            <div className='tableSingle'>
+                <div className='tableHeader'>
+                    <div className='tableTitle'>Inventory List</div>
 
-                        <SearchBar placeholder='Search Item...' />
+                    <img src={addNewBtn} alt='Add New Button' className='addNewBtn' title='Add New Product'
+                         onClick={() => this.addWindowOn()}/>
 
-                        <div id='assignInventory' className='assignInventory' onClick={()=> this.requestWindowOn()}>
+                    <SearchBar placeholder='Search Item...'/>
+
+                    <div id='assignInventory' className='assignInventory' onClick={() => this.requestWindowOn()}>
                         <span className='tulisanAssignInventory'>
                             Request Item
                         </span>
-                        </div>
-
-                        <img src={PrintIcon} id='printIcon' className='printIcon' alt='Print Icon' />
                     </div>
 
-                    <div className='tableBody'>
-                        <table>
-                            <thead>
-                            <tr>
-                                <th className='chkbox'><input type='checkbox' /></th>
-                                <th className='inventory'>Inventory ID</th>
-                                <th className='detail'>Detail</th>
-                                <th className='stock'>Stock</th>
-                                <th className='available'>Available</th>
-                                <th className='price'>Price</th>
-                                <th className='productImage'>Image</th>
-                                <th className='seeUsers'/>
-                                <th className='deleteIcon'/>
-                            </tr>
-                            </thead>
-
-                            <tbody>
-                            {this.state.data.map((item, index)=>(
-                                <tr key={index}>
-                                    <td className='chkbox'><input type='checkbox' name='inventoryId' onChange={()=> {this.addRequest(item.id)}} /></td>
-                                    <td className='inventory'>{item.inventoryId}</td>
-                                    <td className='detail'>{item.detail}</td>
-                                    <td className='stock'>{this.thousandSeparator(item.stock)}</td>
-                                    <td className='available'>{item.available}</td>
-                                    <td className='price'>Rp <span className='moneyValue'>{this.thousandSeparator(item.price)}</span></td>
-                                    <td className='productImage'><img src={image} width='20px' alt='Product'/></td>
-                                    <td className='seeUsers'>See Users</td>
-                                    <td className='deleteIcon' onClick={()=> this.deleteHandler(item.id)}><img src={trashIcon} width='15px' alt='Trash icon' /></td>
-                                </tr>
-                            ))
-                            }
-                            </tbody>
-						</table>
-                    </div>
+                    <img src={PrintIcon} id='printIcon' className='printIcon' alt='Print Icon'/>
                 </div>
-			</div>
-		)
-	}
+
+                <div className='tableBody'>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th className='chkbox'><input type='checkbox'/></th>
+                            <th className='inventory'>Inventory ID</th>
+                            <th className='detail'>Detail</th>
+                            <th className='stock'>Stock</th>
+                            <th className='available'>Available</th>
+                            <th className='price'>Price</th>
+                            <th className='productImage'>Image</th>
+                            <th className='seeUsers'/>
+                            <th className='deleteIcon'/>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        {this.state.data.map((item, index) => (
+                            <tr key={index}>
+                                <td className='chkbox'><input type='checkbox' name='inventoryId' onChange={() => {
+                                    this.addRequest(item.id)
+                                }}/></td>
+                                <td className='inventory'>{item.inventoryId}</td>
+                                <td className='detail'>{item.detail}</td>
+                                <td className='stock'>{Inventory.thousandSeparator(item.stock)}</td>
+                                <td className='available'>{item.available}</td>
+                                <td className='price'>Rp <span
+                                    className='moneyValue'>{Inventory.thousandSeparator(item.price)}</span></td>
+                                <td className='productImage'><img src={image} width='20px' alt='Product'/></td>
+                                <td className='seeUsers'>See Users</td>
+                                <td className='deleteIcon' onClick={() => this.deleteHandler(item.id)}><img
+                                    src={trashIcon}
+                                    width='15px'
+                                    alt='Trash icon'/>
+                                </td>
+                            </tr>
+                        ))
+                        }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    }
 }
