0c4241a25d649a58138f1cff775e4c2fee666bc4
Muhammad Habib <42311916+MHabiib@users.noreply.github.com>
2018-12-31 12:15:29 +0700

    Update Transaction.java

--- future/src/main/java/com/future/model/Transaction.java
+++ future/src/main/java/com/future/model/Transaction.java
@@ -2,6 +2,10 @@ package com.future.model;
 
 public class Transaction {
     String status;
+    String id;
+    
+    public String getId(){return id};
+    public void setId(String id) {this.id=id;}
 
     public String getStatus() {
         return status;
