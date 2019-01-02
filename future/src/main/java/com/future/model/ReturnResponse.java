91f9eacdf9cc1fb12eaff042964d6a5c4ac5e249
Habib <mhabibofficial@yahoo.com>
2019-01-03 03:08:41 +0700

    NEW UPDATE

---
+++ future/src/main/java/com/future/model/ReturnResponse.java
@@ -0,0 +1,13 @@
+package com.future.model;
+
+public class ReturnResponse {
+    private String success;
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(String success) {
+        this.success = success;
+    }
+}
